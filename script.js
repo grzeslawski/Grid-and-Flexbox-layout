7cc633e077f0a8d4c2b1060de4077dd96ea5e082
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2020-02-14 17:31:12 +0100

    add filters of courses in jQuery

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const buttons = document.querySelectorAll(".filters__button");
+// JS
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    let currentButton = document.getElementsByClassName(
-      "filters__button--active"
-    );
-    if (currentButton.length > 0) {
-      currentButton[0].className = currentButton[0].className.replace(
-        " filters__button--active",
-        ""
-      );
-    }
+// const buttons = document.querySelectorAll(".filters__button");
 
-    this.className += " filters__button--active";
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", function() {
+//     let currentButton = document.getElementsByClassName(
+//       "filters__button--active"
+//     );
+//     if (currentButton.length > 0) {
+//       currentButton[0].className = currentButton[0].className.replace(
+//         " filters__button--active",
+//         ""
+//       );
+//     }
+//     this.className += " filters__button--active";
+//   });
+// }
 
-    console.log("działa");
-  });
-}
+// // jQuery
+
+// $(document).ready(function() {
+//   $(".filters__button").click(function() {
+//     $(this).addClass("filters__button--active");
+//     $(".filters__button")
+//       .not(this)
+//       .removeClass("filters__button--active");
+//   });
+// });
+
+$(".filters__button").click(function() {
+  $(".filters__button").removeClass("filters__button--active");
+  $(this).addClass("filters__button--active");
+  const dataLevel = $(this).data("level");
+  console.log(dataLevel);
+
+  if (dataLevel === "all") {
+    $(".item").show();
+  } else {
+    $(".item").hide();
+    $(`[data-level="${dataLevel}"]`).show();
+  }
+});
